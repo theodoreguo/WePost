0c4bded37701eb9bdbbc32e3bd1e1386ecb555cf
theodoreguo <jsguozitong@gmail.com>
2017-01-16 18:46:32 +0800

    Added  dynamically getting namespace function

--- TGWeibo/Classes/Main/MainViewController.swift
+++ TGWeibo/Classes/Main/MainViewController.swift
@@ -15,46 +15,41 @@ class MainViewController: UITabBarController {
         // Set current controller's tab bar color
         tabBar.tintColor = UIColor.orangeColor()
         
-        /*
-        // 1. Create home view controller
-        let home = HomeTableViewController()
-        // 1.1 Set data for home tab bar
-        home.tabBarItem.image = UIImage(named: "tabbar_home")
-        home.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
-        home.tabBarItem.title = "Home"
-        // 1.2 Set data for navigation bar
-        home.navigationItem.title = "Home" // Or use home.title = "Home" instead of the above twe lines
-        
-        // 2. Add navigation controller
-        let nav = UINavigationController()
-        nav.addChildViewController(home)
-        
-        // 3. Add navigation controller to current controller
-        addChildViewController(nav)
-        */
-        
-        addChildViewController(HomeTableViewController(), title: "Home", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "Message", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverTableViewController(), title: "Discover", imageName: "tabbar_discover")
-        addChildViewController(MeTableViewController(), title: "Me", imageName: "tabbar_profile")
+        // Add child view controllers
+        addChildViewController("HomeTableViewController", title: "Home", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "Message", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "Discover", imageName: "tabbar_discover")
+        addChildViewController("MeTableViewController", title: "Me", imageName: "tabbar_profile")
     }
     
     /**
      Initialize child view controller
      
-     - parameter childController: child controller that needs initalization
-     - parameter title:           child controller's title
-     - parameter imageName:       child controller's image
+     - parameter childControllerName: child controller that needs initalization
+     - parameter title:               child controller's title
+     - parameter imageName:           child controller's image
      */
-    private func addChildViewController(childController: UIViewController, title: String, imageName: String) {
-        // 1. Set data for home tab bar
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        childController.title = title
+    private func addChildViewController(childControllerName: String, title: String, imageName: String) {
+        // Acquire namespace dynamicly (Namespace is included in the class name when creating an object using stipulated class name)
+        let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        // Convert string to class
+        // Namespace is project's name by default, but it can be modified
+        let className:AnyClass? = NSClassFromString(nameSpace + "." + childControllerName)
+        // Create the object according to the corresponding class
+        // Convert AnyClass to specified type
+        let viewControllerClass = className as! UIViewController.Type
+        // Create the object
+        let viewController = viewControllerClass.init()
+        
+        // 1. Set data for tab bar
+        viewController.tabBarItem.image = UIImage(named: imageName)
+        viewController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        viewController.title = title
         
         // 2. Add navigation controller
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(viewController)
         
         // 3. Add navigation controller to current controller
         addChildViewController(nav)
